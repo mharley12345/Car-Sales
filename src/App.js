6572c9e4dca88e5353007a595f495f17d221ab6b
Mike Harley <mharley12345@gmail.com>
2019-09-11 15:44:30 -0700

    Nope

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
-import React,{Component} from 'react';
-
+import React from 'react';
+import  connnect from 'react-redux'
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+import {removeFeature, addFeature} from './actions'
 
 
-class App extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      car:''
-    };
+function App(){
+  
 
-    const removeFeature = (feature) => {
-    feature.props.removeFeature(feature);
-    return removeFeature(this.feature)
+  removeFeature = (feature) => {
+    this.dispatch({type:"REMOVE_FEATURE" , payload:feature})
   }
+     
+  
 
   const buyItem = props => {
     // dipsatch an action here to add an item
   }
-};
-render(){
+
+
   return (
     <div className="boxes">
       <div className="box">
         <Header />
-        <AddedFeatures removeFeature={this.removeFeature} feature={this.state.feature}/>
+        <AddedFeatures/>
       </div>
       <div className="box">
         <AdditionalFeatures  />
@@ -35,7 +33,5 @@ render(){
       </div>
     </div>
   );
-};
-}
-
+  }
 export default App;
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,12 @@
+
+export const ADD_FEATURE = 'ADD_FEATURE'
 export const addFeature = feature => {
     return {
         type:'ADD_FEATURE',
         payload:feature
     }
 }
+export const REMOVE_FEATURE = 'REMOVE_FEATURE';
 export const removeFeature = feature => {
     console.log("remove called");
     return {
